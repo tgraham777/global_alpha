64a6bbd1fe1a65b1523bb70170c2bab918101081
Tyler Graham <tylergraham134@gmail.com>
2016-03-11 15:09:52 -0700

    image slideshow on landing now working

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
-  setTimeout(function() {
-    var pic = $("#img1");
-    pic[0].src = "https://www.directinvesting.com/InvestingImage.jpg";
+
+$(document).ready(function(){
+  var slideimages = new Array()
+  slideimages[0] = new Image()
+  slideimages[0].src = "/images/img1.jpg"
+  slideimages[1] = new Image()
+  slideimages[1].src = "/images/img2.jpg"
+  slideimages[2] = new Image()
+  slideimages[2].src = "/images/img3.jpg"
+
+  var step = 1;
+
+  setInterval(function(){
+    $('img')[0].src = slideimages[step].src;
+
+    if(step < 2) {
+      step++;
+    } else {
+      step = 0;
+    }
   }, 3000);
-})
+
+  // debugger;
+});
