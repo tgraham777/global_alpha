757e9c71857f5375eee8fb2ecc6e3144ee50f0f3
Tyler Graham <tylergraham134@gmail.com>
2016-04-05 13:50:00 -0600

    initializes bootstrap and puts bootstrap carousel in place on landing

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,7 @@
 class LandingController < ApplicationController
   def show
   end
+
+  def index    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'landing#show'
+  root 'landing#index'
 
   resources :users, only: [:new, :create, :show]
   get '/login', to: 'sessions#new'
