b40352348b3d841eb7dcc314e4952a424ccd702a
Tyler Graham <tylergraham134@gmail.com>
2016-04-08 15:51:36 -0600

    tags and topic creation now working

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :topic_tags
   has_many :tags, through: :topic_tags
+
+  validates_presence_of :title
+  validates_presence_of :body
+
+  # accepts_nested_attributes_for :tags
 end
