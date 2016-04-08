ede87d66d0d0e26510a726c59e38149830a744ba
Tyler Graham <tylergraham134@gmail.com>
2016-04-08 16:23:41 -0600

    reformats navbars on each page and minor formatting changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     if !current_user
-      flash[:error] = "You need to log in first!"
+      flash[:error] = "You need to log in or create an account first!"
       redirect_to login_path
     end
   end
