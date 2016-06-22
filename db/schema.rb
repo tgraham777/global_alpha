7a20108790164805fb85c8a53f3d762de46dada4
Tyler Graham <tylergraham134@gmail.com>
2016-06-22 13:22:06 -0600

    removes visual_link column from topics table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622182236) do
+ActiveRecord::Schema.define(version: 20160622192033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,9 +83,8 @@ ActiveRecord::Schema.define(version: 20160622182236) do
   create_table "topics", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.text     "visual_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
