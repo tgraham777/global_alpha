559b0f0e513690cd416b65bfcb59aa601e995f16
Tyler Graham <tylergraham134@gmail.com>
2016-06-22 16:01:53 -0600

    Add Visual button now appends base topic elements to the page - numbers still unchanged

--- app/assets/javascripts/topic_creator.js
+++ app/assets/javascripts/topic_creator.js
@@ -1,50 +1,5 @@
 $(document).ready(function(){
-  
-
-  var step = 1;
-
-  var interval = setInterval(function(){
-    if(step === 1){
-      $('#img1').fadeOut(1500);
-      step++;
-    } else if(step === 2){
-      $('#img2').fadeOut(1500);
-      step = 0;
-    } else if(step === 0){
-      $('#img1').fadeIn(1500);
-      $('#img2').fadeIn(1500);
-      step++;
-    }
-
-    changeButtonOpacity();
-  }, 7000);
-
-  $('#left-arrow').click(function(event){
-    moveLeft();
-    changeButtonOpacity();
-    return false;
-  })
-
-  $('#right-arrow').click(function(event){
-    moveRight();
-    changeButtonOpacity();
-    return false;
-  })
-
-  function moveRight(){
-    clearInterval(interval);
-    if(step === 1){
-      $('#img2').fadeIn(1500);
-      $('#img1').fadeOut(1500);
-      step++;
-    } else if(step === 2){
-      $('#img3').fadeIn(1500);
-      $('#img2').fadeOut(1500);
-      step = 0;
-    } else if(step === 0){
-      $('#img1').fadeIn(1500);
-      $('#img3').fadeOut(1500);
-      step++;
-    }
-  }
+  $('#add-visual-button').click(function(){
+    $('.default-topic-items').append('<br><label for="visual_link_1">Visual link 1</label><input type="visual_link_1" name="visual_link_1" id="visual_link_1" class="form-control" placeholder="Enter link or URL"><br><label for="caption_1">Caption 1</label><input type="caption_1" name="caption_1" id="caption_1" class="form-control" placeholder="Enter caption for visual"><br><label for="description_1">Description 1</label><input type="description_1" name="description_1" id="description_1" class="form-control" placeholder="Enter description for visual and/or additional text"><br>');
+  });
 });
