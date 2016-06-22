790557d47a9c43164da3255e22199e729cd39df7
Tyler Graham <tylergraham134@gmail.com>
2016-06-22 15:01:54 -0600

    adds intro column to topics table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622205823) do
+ActiveRecord::Schema.define(version: 20160622210027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160622205823) do
     t.text     "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "intro"
   end
 
   create_table "users", force: :cascade do |t|
