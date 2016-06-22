458c92bc2f483768659680e9806ab2978183a008
Tyler Graham <tylergraham134@gmail.com>
2016-06-22 15:06:54 -0600

    adds report_date column to topics table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622210216) do
+ActiveRecord::Schema.define(version: 20160622210429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,10 +82,11 @@ ActiveRecord::Schema.define(version: 20160622210216) do
 
   create_table "topics", force: :cascade do |t|
     t.text     "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.text     "intro"
     t.text     "conclusion"
+    t.string   "report_date"
   end
 
   create_table "users", force: :cascade do |t|
