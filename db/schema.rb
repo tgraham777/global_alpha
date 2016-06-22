71e53be9518c01c101d23d608b3400b38a6a5b89
Tyler Graham <tylergraham134@gmail.com>
2016-06-22 12:24:57 -0600

    adds visual_link column to topics table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620202444) do
+ActiveRecord::Schema.define(version: 20160622182236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,8 +83,9 @@ ActiveRecord::Schema.define(version: 20160620202444) do
   create_table "topics", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "visual_link"
   end
 
   create_table "users", force: :cascade do |t|
