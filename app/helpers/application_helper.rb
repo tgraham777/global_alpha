d182e1bc0c797b1e09f12eda1f21eeb5934d2d7f
Tyler Graham <tylergraham134@gmail.com>
2016-07-18 11:57:44 -0600

    heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,22 +13,22 @@ module ApplicationHelper
     nil
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section."
-      redirect_to '/login'
-    end
-  end
-
-  def logged_in?
-    current_user != nil
-  end
-
-  def current_admin?
-    current_user && current_user.admin?
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  #
+  # def require_login
+  #   unless logged_in?
+  #     flash[:error] = "You must be logged in to access this section."
+  #     redirect_to '/login'
+  #   end
+  # end
+  #
+  # def logged_in?
+  #   current_user != nil
+  # end
+  #
+  # def current_admin?
+  #   current_user && current_user.admin?
+  # end
 end
