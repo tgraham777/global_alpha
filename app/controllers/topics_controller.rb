ecb96d58f96f9377ec0b53f880825a21e7cf0e2b
Tyler Graham <tylergraham134@gmail.com>
2016-09-26 14:23:19 -0600

    sorts related topics by updated_at on admin and non-admin topic show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,6 +10,6 @@ class TopicsController < ApplicationController
     @visuals = @topic.visuals.sort
     @related_countries = @topic.tags.sample.countries.last(3)
     @related_indicators = @topic.tags.sample.indicators.last(3)
-    @related_topics = @topic.tags.sample.topics.reject{ |topic| topic.title == @topic.title }.last(2)
+    @related_topics = @topic.tags.sample.topics.reject{ |topic| topic.title == @topic.title }.sort_by(&:updated_at).last(2).reverse!
   end
 end
