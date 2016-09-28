c8a64ee6936a804a8ac1fcf0f2a272187fcd0ec3
Tyler Graham <tylergraham134@gmail.com>
2016-09-28 16:36:12 -0600

    puts reverses back in for related topic controller actions

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -8,6 +8,6 @@ class IndicatorsController < ApplicationController
   def show
     @indicator = Indicator.find_by(display_name: params[:display_name])
     @related_countries = Country.all
-    @related_topics = @indicator.tags.sample.topics.last(2)
+    @related_topics = @indicator.tags.sample.topics.last(2).reverse!
   end
 end
