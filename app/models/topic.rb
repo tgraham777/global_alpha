bc0fb91088b513151011d4bc00d269dce77996ef
Tyler Graham <tylergraham134@gmail.com>
2016-10-18 13:21:33 -0600

    creates topic_countries relationship

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,6 +5,8 @@ class Topic < ActiveRecord::Base
   has_many :visuals, through: :topic_visuals
   has_many :topic_indicators
   has_many :indicators, through: :topic_indicators
+  has_many :topic_countries
+  has_many :countries, through: :topic_countries
 
   accepts_nested_attributes_for :visuals
 
