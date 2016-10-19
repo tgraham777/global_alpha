f9a74175be4f01b1e4372a100f06785f29e65ea5
Tyler Graham <tylergraham134@gmail.com>
2016-10-19 16:11:04 -0600

    completes country create and update functionality and initial show page layout

---
+++ public/assets/topic_creator-a9d7361c13a996eed77c3a0927ce2a81b084bc500486deb0287b61485118f21e.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  var count = 1;
+
+  $('#add-visual-button').click(function(){
+    visual_number = count + 1;
+    $('.default-topic-items').append('<h4>Visual #' + visual_number + '</h4><label for="topic_visuals_attributes_' + count + '_link">Link</label><input class="form-control" placeholder="Enter Tableau script or URL" type="text" name="topic[visuals_attributes][' + count + '][link]" id="topic_visuals_attributes_' + count + '_link"><br><label for="topic_visuals_attributes_' + count + '_caption">Caption</label><input class="form-control" placeholder="Enter caption for visual" type="text" name="topic[visuals_attributes][' + count + '][caption]" id="topic_visuals_attributes_' + count + '_caption"><br><label for="topic_visuals_attributes_' + count + '_description">Description</label><textarea class="form-control" placeholder="Enter description for visual and/or additional text" name="topic[visuals_attributes][' + count + '][description]" id="topic_visuals_attributes_' + count + '_description"></textarea><br><hr>');
+    count++;
+  });
+
+  var visual_count = $('.edit-page-visual-counter').length;
+
+  $('#edit-add-visual-button').click(function(){
+    $('.default-topic-items').append('<hr><label for="topic_visuals_attributes_' + visual_count + '_link">Link</label><input class="form-control" type="text" value="" name="topic[visuals_attributes][' + visual_count + '][link]" id="topic_visuals_attributes_' + visual_count + '_link"><br><label for="topic_visuals_attributes_' + visual_count + '_caption">Caption</label><input class="form-control" type="text" value="" name="topic[visuals_attributes][' + visual_count + '][caption]" id="topic_visuals_attributes_' + visual_count + '_caption"><br><label for="topic_visuals_attributes_' + visual_count + '_description">Description</label><textarea class="form-control" name="topic[visuals_attributes][' + visual_count + '][description]" id="topic_visuals_attributes_' + visual_count + '_description"></textarea><br>');
+    visual_count++;
+  });
+
+  $('#country-add-visual-button').click(function(){
+    visual_number = count + 1;
+    $('.default-country-items').append('<h4>Visual #' + visual_number + '</h4><label for="country_visuals_attributes_' + count + '_link">Link</label><input class="form-control" placeholder="Enter Tableau script or URL" type="text" name="country[visuals_attributes][' + count + '][link]" id="country_visuals_attributes_' + count + '_link"><br><label for="country_visuals_attributes_' + count + '_caption">Caption</label><input class="form-control" placeholder="Enter caption for visual" type="text" name="country[visuals_attributes][' + count + '][caption]" id="country_visuals_attributes_' + count + '_caption"><br><label for="country_visuals_attributes_' + count + '_description">Description</label><textarea class="form-control" placeholder="Enter description for visual and/or additional text" name="country[visuals_attributes][' + count + '][description]" id="country_visuals_attributes_' + count + '_description"></textarea><br><hr>');
+    count++;
+  });
+
+  var country_visual_count = $('.country-edit-page-visual-counter').length;
+
+  $('#country-edit-add-visual-button').click(function(){
+    $('.default-country-edit-items').append('<hr><label for="country_visuals_attributes_' + country_visual_count + '_link">Link</label><input class="form-control" type="text" value="" name="country[visuals_attributes][' + country_visual_count + '][link]" id="country_visuals_attributes_' + country_visual_count + '_link"><br><label for="country_visuals_attributes_' + country_visual_count + '_caption">Caption</label><input class="form-control" type="text" value="" name="country[visuals_attributes][' + country_visual_count + '][caption]" id="country_visuals_attributes_' + country_visual_count + '_caption"><br><label for="country_visuals_attributes_' + country_visual_count + '_description">Description</label><textarea class="form-control" name="country[visuals_attributes][' + country_visual_count + '][description]" id="country_visuals_attributes_' + country_visual_count + '_description"></textarea><br>');
+    country_visual_count++;
+  });
+});
