93c5cdaf0907c7ecddcaee977b02bb5aff421f5e
Tyler Graham <tylergraham134@gmail.com>
2016-10-24 15:46:21 -0600

    visual edit functionality now working properly

--- app/controllers/admin/visuals_controller.rb
+++ app/controllers/admin/visuals_controller.rb
@@ -4,4 +4,24 @@ class Admin::VisualsController < Admin::BaseController
   def show
     @visual = Visual.find_by(display_name: params[:display_name])
   end
+
+  def edit
+    @visual = Visual.find_by(display_name: params[:display_name])
+  end
+
+  def update
+    @visual = Visual.find_by(display_name: params[:display_name])
+    if @visual.update(visual_params)
+      flash[:success] = "Visual was updated successfully!"
+      redirect_to admin_visual_path(@visual)
+    else
+      flash[:error] = @visual.errors.full_messages.first
+      redirect_to edit_admin_visual_path(@visual)
+    end
+  end
+
+private
+  def visual_params
+    params.require(:visual).permit(:title, :link, :caption, :description, :additional_description)
+  end
 end
