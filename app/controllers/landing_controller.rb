10a0105bfe1196a2c9d3cdae6ec631c0bb1a119f
Tyler Graham <tylergraham134@gmail.com>
2016-11-04 12:48:28 -0600

    links new preview titles to landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,8 +3,8 @@ class LandingController < ApplicationController
   end
 
   def index
-    @first_preview = Preview.find_by(title: "Hot Topic")
-    @second_preview = Preview.find_by(title: "Global Macro")
-    @third_preview = Preview.find_by(title: "Financial Markets")
+    @first_preview = Preview.find_by(title: "The Global Oil Market")
+    @second_preview = Preview.find_by(title: "Foreign Holdings of U.S. Debt")
+    @third_preview = Preview.find_by(title: "Emerging Markets Debt")
   end
 end
