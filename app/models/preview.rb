34cee300aed06d70a659523c1d990e5b46b38570
Tyler Graham <tylergraham134@gmail.com>
2016-11-04 12:14:04 -0600

    modifies Preview model and routes to account for change from name to title

--- app/models/preview.rb
+++ app/models/preview.rb
@@ -2,9 +2,10 @@ class Preview < ActiveRecord::Base
   has_many :preview_tags, dependent: :delete_all
   has_many :tags, through: :preview_tags
 
-  validates :name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+  validates_presence_of :report_date
 
   def to_param
-    name
+    title
   end
 end
