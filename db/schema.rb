4e06379079ae1726cd3ceb80411f247a1cb63d1e
Tyler Graham <tylergraham134@gmail.com>
2016-11-07 22:08:14 -0700

    adds tagline to Preview table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108050120) do
+ActiveRecord::Schema.define(version: 20161108050732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(version: 20161108050120) do
     t.text     "intro"
     t.text     "conclusion"
     t.string   "report_date"
+    t.text     "tagline"
   end
 
   create_table "tags", force: :cascade do |t|
