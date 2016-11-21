e054cd56344fce2688eaed94f19c1467a8f5b0d4
Tyler Graham <tylergraham134@gmail.com>
2016-11-21 14:51:19 -0700

    adjusts non-admin preview show and index pages to look like topic

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -6,8 +6,6 @@ class PreviewsController < ApplicationController
   def show
     @preview = Preview.find_by(display_name: params[:display_name])
     @visuals = @preview.visuals.sort
-    @related_topics = @preview.topics.sort_by(&:updated_at).last(2).reverse!
-    @related_countries = @preview.countries
-    @related_indicators = @preview.indicators
+    @related_previews = @preview.tags.sample.previews.reject{ |preview| preview.title == @preview.title }.sort_by(&:updated_at).last(2).reverse!
   end
 end
