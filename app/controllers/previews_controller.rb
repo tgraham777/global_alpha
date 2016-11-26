9ce09818c3ab594b857a06743e94ccfbc71b4aa6
Tyler Graham <tylergraham134@gmail.com>
2016-11-25 19:31:44 -0700

    when guest logged in preview show now actually links to topics at bottom

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -6,6 +6,7 @@ class PreviewsController < ApplicationController
   def show
     @preview = Preview.find_by(display_name: params[:display_name])
     @visuals = @preview.visuals.sort
+    @related_topics = @preview.topics.sort_by(&:updated_at).last(2).reverse!
     @related_previews = @preview.find_related_previews
   end
 end
